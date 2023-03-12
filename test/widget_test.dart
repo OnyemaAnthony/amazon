abed8b8a6e7a96116305530f53f02d5841a1669b
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-03-12 20:34:03 +0100

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:amazon/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -25,6 +25,7 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
+    expect(find.text('2'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
 }
